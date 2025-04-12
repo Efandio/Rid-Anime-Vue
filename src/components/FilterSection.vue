c998df8720ee1015f0089b4c4c5c489466b2971d
Efandio <efandio904@gmail.com>
2025-04-12 12:25:41 +0700

    adding select components

---
+++ src/components/FilterSection.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from './ui/button/Button.vue';
+</script>
+
+<template>
+    <div class="grid grid-cols-2">
+        <Button></Button>
+        <Button></Button>
+    </div>
+</template>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import FilterSection from '@/components/FilterSection.vue';
 </script>
 
 <template>
     <main class="">
-        
+        <div class="w-full h-[30vh]">
+            <FilterSection />
+        </div>
     </main>
 </template>
