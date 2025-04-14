e63bd120eeeff7c5214448fed459462ba8027851
Efandio <efandio904@gmail.com>
2025-04-14 13:24:54 +0700

    see more trending anime

--- src/components/mainPage/TrendingNowSection.vue
+++ src/components/mainPage/TrendingNowSection.vue
@@ -20,7 +20,7 @@ const mobile = useIsMobile();
 </script>
 
 <template>
-    <main>
+    <main class="w-full flex flex-col">
         <div class="w-full flex justify-between px-2 lg:px-20">
             <Badge class="">Trending Now</Badge>
             <RouterLink to="/trending-now">
@@ -29,13 +29,13 @@ const mobile = useIsMobile();
         </div>
         <div v-if="loading">Loading...</div>
         <div v-if="error">Error...</div>
-        <div v-if="mobile" class="flex overflow-x-auto gap-5 w-full justify-center mt-2">
+        <div v-if="mobile" class="flex gap-5 w-full justify-center mt-2">
             <Card class="w-42 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white cursor-pointer" v-for="anime in trendingAnime.trendingAnimeList?.Page.media.slice(0, 3)" :key="anime.id">
                 <img class="w-20 h-32" :src="anime.coverImage.large || undefined" alt="">
                 <h1 class="text-sm font-medium">{{ anime.title.english?.slice(0, 15) + '...' }}</h1>
             </Card>
         </div>
-        <div v-else class="flex overflow-x-auto gap-5 w-full justify-center mt-4">
+        <div v-else class="flex gap-5 w-full h-auto justify-center mt-4">
             <Card class="w-42 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white cursor-pointer" v-for="anime in trendingAnime.trendingAnimeList?.Page.media.slice(0, 6)" :key="anime.id">
                 <img class="lg:w-32 lg:h-48" :src="anime.coverImage.large || undefined" alt="">
                 <h1 class="text-sm font-medium">{{ anime.title.english?.slice(0, 30) + '...' }}</h1>
--- src/views/TrendingNowPage.vue
+++ src/views/TrendingNowPage.vue
@@ -1,10 +1,41 @@
 
 <script setup lang="ts">
+    import { useTrendingAnime } from '@/stores/animeStore';
+    import { onMounted } from 'vue';
+    import { Card } from '@/components/ui/card';
+    import { useIsMobile } from '@/composables/isMobile';
+
+    const mobile = useIsMobile();
+    const allTrendingAnime = useTrendingAnime();
+
+    const loading = allTrendingAnime.loading;
+    const error = allTrendingAnime.error;
+
+    onMounted(async() => {
+        allTrendingAnime.trendingAnime()
+    });
 
 </script>
 
 <template>
-    <div>
-        ss
+    <div class="pt-28 bg-black h-auto">
+        <div v-if="loading">Loading...</div>
+        <div v-if="error">Error...</div>
+
+        <section class="w-full h-auto">
+        <div v-if="mobile" class="grid grid-cols-3 justify-items-center mt-4">
+            <Card class="w-36 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white cursor-pointer border-none" v-for="anime in allTrendingAnime.trendingAnimeList?.Page.media" :key="anime.id">
+                <img class="w-28 h-36 rounded-lg" :src="anime.coverImage.large || undefined" alt="">
+                <h1 class="text-sm font-medium -translate-y-3">{{ anime.title.english?.slice(0, 15) + '...' }}</h1>
+            </Card>
+        </div>
+        <div v-else class="grid grid-cols-7 w-full justify-center mt-4 px-10">
+            <Card class="w-42 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white cursor-pointer border-none" v-for="anime in allTrendingAnime.trendingAnimeList?.Page.media" :key="anime.id">
+                <img class="lg:w-64 lg:h-52 rounded-lg" :src="anime.coverImage.large || undefined" alt="">
+                <h1 class="text-sm font-medium -translate-y-4">{{ anime.title.english?.slice(0, 30) + '...' }}</h1>
+            </Card>
+        </div>
+        </section>
     </div>
 </template>
+
