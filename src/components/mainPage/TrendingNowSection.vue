ebbce5650e974abe95e3af46846596046dce6406
Efandio <efandio904@gmail.com>
2025-04-14 11:02:49 +0700

    trending anime store

--- src/components/mainPage/TrendingNowSection.vue
+++ src/components/mainPage/TrendingNowSection.vue
@@ -1,35 +1,21 @@
 <script setup lang="ts">
 
-import { onMounted, reactive } from 'vue';
+import { useTrendingAnime } from '@/stores/animeStore';
+import { onMounted } from 'vue';
 import Badge from '../ui/badge/Badge.vue';
 import Button from '../ui/button/Button.vue';
 import { Card } from '../ui/card';
-import { animeFetcher } from '@/api/fetcher';
-import { trendingNowQuery, trendingNowQueryVariables } from '@/api/queries/trendingNow';
-import type { TrendingAnimeType } from '@/api/queries/trendingNow';
 import { useIsMobile } from '@/composables/isMobile';
 
-const data = reactive({
-    anime: null as TrendingAnimeType | null,
-    loading: true,
-    error: null as Error | null,
+const trendingAnime = useTrendingAnime();
+const loading = trendingAnime.loading;
+const error = trendingAnime.error;
+
+onMounted( async() => {
+    await trendingAnime.trendingAnime()
 })
 
-onMounted( async () => {
-    try {
-        const res = await animeFetcher({
-            query: trendingNowQuery,
-            variables: trendingNowQueryVariables
-        })
-        data.anime = res;
-        data.loading = false;
-    } catch (err) {
-        data.error = err as Error;
-        data.loading = false
-    }
-});
-
-const mobile = useIsMobile()
+const mobile = useIsMobile();
 
 </script>
 
@@ -39,16 +25,16 @@ const mobile = useIsMobile()
             <Badge class="">Trending Now</Badge>
             <Button class="text-xs cursor-pointer">See All</Button>
         </div>
-        <div v-if="data.loading">Loading...</div>
-        <div v-if="data.error">Error...</div>
+        <div v-if="loading">Loading...</div>
+        <div v-if="error">Error...</div>
         <div v-if="mobile" class="flex overflow-x-auto gap-5 w-full justify-center mt-2">
-            <Card class="w-42 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white" v-for="anime in data.anime?.Page.media.slice(0, 3)" :key="anime.id">
+            <Card class="w-42 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white" v-for="anime in trendingAnime.trendingAnimeList?.Page.media.slice(0, 3)" :key="anime.id">
                 <img class="w-20 h-32" :src="anime.coverImage.large || undefined" alt="">
                 <h1 class="text-sm">{{ anime.title.english?.slice(0, 15) + '...' }}</h1>
             </Card>
         </div>
         <div v-else class="flex overflow-x-auto gap-5 w-full justify-center mt-4">
-            <Card class="w-42 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white" v-for="anime in data.anime?.Page.media.slice(0, 6)" :key="anime.id">
+            <Card class="w-42 grid grid-cols-1 justify-items-center px-2 bg-transparent text-white" v-for="anime in trendingAnime.trendingAnimeList?.Page.media.slice(0, 6)" :key="anime.id">
                 <img class="lg:w-32 lg:h-48" :src="anime.coverImage.large || undefined" alt="">
                 <h1 class="text-sm">{{ anime.title.english?.slice(0, 30) + '...' }}</h1>
             </Card>
--- src/stores/animeStore.ts
+++ src/stores/animeStore.ts
@@ -1,11 +1,41 @@
+import { animeFetcher } from "@/api/fetcher";
+import type { TrendingAnimeType } from "@/api/queries/trendingNow";
 import { defineStore } from "pinia";
+import { trendingNowQuery, trendingNowQueryVariables } from "@/api/queries/trendingNow";
 
 export const useTrendingAnime = defineStore('trending', {
 state: () => {
-    return null
+    return {
+        trendingAnimeList: null as TrendingAnimeType | null,
+        loading: false,
+        error: null as string | null
+    }
 },
 
 actions: {
-    
-}
+    async trendingAnime() {
+        this.loading = true;
+        try {
+            const response = await animeFetcher({
+                query: trendingNowQuery,
+                variables: trendingNowQueryVariables
+            })
+            if (response && response.Page && response.Page.media) {
+                this.trendingAnimeList = response;
+            } else {
+                throw new Error ('Invalid Response Structure')
+            }
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                this.error = err.message;
+            } else {
+                this.error = String(err)
+            }
+        } finally {
+            this.loading = false;
+        }
+    }
+},
+
+
 })
